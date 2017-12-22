6642c689d2b7608d151ef23324aea5a9ad6e46a0
Timothy Spann <tspann@hortonworks.com>
2017-12-22 16:01:17 -0500

    Create meterreader.py

---
+++ meterreader.py
@@ -0,0 +1,39 @@
+from pyHS100 import SmartPlug, SmartBulb
+#from pprint import pformat as pf
+import json
+import datetime
+
+plug = SmartPlug("192.168.1.200")
+
+row = { }
+
+emeterdaily = plug.get_emeter_daily(year=2017, month=12)
+for k, v in emeterdaily.items():
+     row["hour%s" % k] = v
+
+hwinfo = plug.hw_info
+for k, v in hwinfo.items():
+     row["%s" % k] = v
+
+sysinfo = plug.get_sysinfo()
+for k, v in sysinfo.items():
+     row["%s" % k] = v
+
+timezone = plug.timezone
+for k, v in timezone.items():
+     row["%s" % k] = v
+
+emetermonthly =  plug.get_emeter_monthly(year=2017)
+for k, v in emetermonthly.items():
+     row["day%s" % k] = v
+
+realtime = plug.get_emeter_realtime()
+for k, v in realtime.items():
+     row["%s" % k] = v
+
+row['alias'] = plug.alias
+row['time'] =  plug.time.strftime('%m/%d/%Y %H:%M:%S')
+row['ledon'] =  plug.led
+row['systemtime'] = datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')
+json_string = json.dumps(row)
+print(json_string)
